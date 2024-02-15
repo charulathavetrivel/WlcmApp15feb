0280685535f14aeb9614eb377e1de73b037bc81d
Charulatha <charulatha200701@gmail.com>
2024-02-15 12:08:57 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WlcmApp15feb;
+
+Console.WriteLine("Welcome to Authentication App");
+SignIn obj = new SignIn();
+Console.WriteLine(obj.Authenticate("admin", "admin@123"));
+
---
+++ SignIn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WlcmApp15feb
+{
+    public class SignIn
+    {
+        public string Authenticate(string username, string password)
+        {
+            string msg;
+            if((string.IsNullOrEmpty(username)) || string.IsNullOrEmpty(password))
+            {
+                msg = "username and password required";
+            }
+            else
+            {
+                if((username == "admin") && (password == "admin@123"))
+                {
+                    msg = "Authentication Pass";
+                }
+                else
+                {
+                    msg = "Authentication Fail";
+                }
+
+            }
+            return msg;
+
+        }
+    }
+}
